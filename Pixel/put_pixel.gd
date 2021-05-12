7297f8c02710c554008c4eba67715aee526485f8
Icad <irsyadmuhammad.020201@gmail.com>
2021-05-12 11:58:05 +0700

    Pixel class

--- Pixel/put_pixel.gd
+++ Pixel/put_pixel.gd
@@ -4,20 +4,21 @@ class_name put_pixel
 
 
 func put_pixel(
-	# Represent of x point
-	x, 
-	# Represent of y point
-	y, 
-	# Argument for receiving the color of the point
-	color
-):
-	# initialize color
+	
+	x,        # Represent of x point 
+	y,        # Represent of y point
+	color     # Argument for receiving the color of the point
+	
+	):
+	
+	# Initialize color
 	var points_color = PoolColorArray()
 	points_color.push_back(color);
 	
-	# intialize dot
+	# Intialize dot
 	var points_dot = PoolVector2Array()
 	points_dot.push_back(Vector2(x,y))
 	
-	# draw the dot
+	# Draw the dot
 	draw_primitive(points_dot, points_color, [])
+
